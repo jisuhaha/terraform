f63529e79563883e498d42f84b5fa6461b2e4772
Jisu Han <sysyell3@naver.com>
2024-03-22 12:18:38 +0900

    0.07 add rds

--- jisu/rds.tf
+++ jisu/rds.tf
@@ -1,32 +1,15 @@
-/*
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "MenuSelection"
-  engine               = "maridb"
-  engine_version       = "10.11.6"
-  instance_class       = "db.m5.large"
-  username             = "foo"
-  password             = "foobarbaz"
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.rds-subnet
-  vpc_security_group_ids = [aws_db_subnet_group.rds-subnet]
-
-}
-
-
-*/
 resource "aws_db_instance" "project-rds" {
-  identifier          = "project-RDS"
+  identifier          = "projectrds"
   multi_az             = true
   allocated_storage    = 10
   storage_type         = "standard"
   db_name              = "MenuSelection"
-  engine               = "maridb"
+  engine               = "MariaDB"
   engine_version       = "10.11.6"
   instance_class       = "db.m5.large"
   username             = "dbadmin"
   password             = "dbmypassword"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.rds-subnet.name
-  vpc_security_group_ids = [aws_db_subnet_group.rds-subnet.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 }
